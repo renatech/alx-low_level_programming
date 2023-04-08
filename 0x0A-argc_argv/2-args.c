95a94866c339c7d4818b78c69f37b56a0a8f7bc8
Rena <samuelserenaluv@gmail.com>
2023-04-07 17:54:01 -0700

    m

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,10 +9,10 @@
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
-{       
-        int i;
-        
-        for (i = 0; i < argc; i++);
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
